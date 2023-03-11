cc860e5a2e9e495059934c9d611964885a462173
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-03-11 23:45:23 +0200

    Create Screens folder and files

--- react-native-proj/App.js
+++ react-native-proj/App.js
@@ -5,29 +5,52 @@ import {
   View,
   ImageBackground,
   TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
 } from "react-native";
-
-// const bgImage = require("./assets/bg.avif");
+import { useState } from "react";
 
 export default function App() {
+  const [isKeyboardShown, setIsKeyboardShown] = useState(false);
   return (
     <View style={styles.container}>
       <ImageBackground
         style={styles.imageBG}
-        source={require("./assets/ice-cream-1-min.jpg")}
+        source={require("./assets/bg-image.jpg")}
       >
-        <View style={styles.form}>
-          <Text style={styles.titleForm}>Registration form</Text>
-          <View style={{ marginBottom: 20 }}>
-            <Text style={styles.inputLabel}>Email</Text>
-            <TextInput style={styles.input} />
+        <KeyboardAvoidingView
+          style={{ flex: 1, justifyContent: "center" }}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+          <View
+            style={{
+              ...styles.form,
+              marginBottom: isKeyboardShown ? 20 : 100,
+            }}
+          >
+            <Text style={styles.titleForm}>Registration form</Text>
+            <View style={{ marginBottom: 20 }}>
+              <Text style={styles.inputLabel}>Email</Text>
+              <TextInput
+                style={styles.input}
+                onFocus={() => setIsKeyboardShown(true)}
+              />
+            </View>
+            <View style={{ marginBottom: 40 }}>
+              <Text style={styles.inputLabel}>Password</Text>
+              <TextInput
+                style={styles.input}
+                secureTextEntry={true}
+                onFocus={() => setIsKeyboardShown(true)}
+              />
+            </View>
+            <TouchableOpacity style={styles.btn} activeOpacity={0.8}>
+              <Text style={styles.btnText}>SIGN IN</Text>
+            </TouchableOpacity>
           </View>
-          <View>
-            <Text style={styles.inputLabel}>Password</Text>
-            <TextInput style={styles.input} />
-          </View>
-        </View>
-        <StatusBar style="auto" />
+          <StatusBar style="auto" />
+        </KeyboardAvoidingView>
       </ImageBackground>
     </View>
   );
@@ -41,7 +64,7 @@ const styles = StyleSheet.create({
   imageBG: {
     flex: 1,
     resizeMode: "containe",
-    justifyContent: "center",
+    justifyContent: "flex-end",
     // alignItems: "center",
   },
   titleForm: {
@@ -52,7 +75,7 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(225,225,225, 0.8)",
   },
   form: {
-    marginHorizontal: 20,
+    marginHorizontal: 30,
   },
   inputLabel: {
     fontSize: 18,
@@ -64,7 +87,34 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 2,
     backgroundColor: "white",
-    fontSize: 20,
+    fontSize: 18,
     padding: 10,
+  },
+  btn: {
+    marginHorizontal: 20,
+    borderRadius: 10,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    ...Platform.select({
+      ios: {
+        backgroundColor: "#fff",
+      },
+      android: {
+        backgroundColor: "green",
+      },
+    }),
+  },
+  btnText: {
+    fontSize: 18,
+    fontWeight: 500,
+    ...Platform.select({
+      ios: {
+        color: "green",
+      },
+      android: {
+        color: "#fff",
+      },
+    }),
   },
 });
